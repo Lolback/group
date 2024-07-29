f9814f0eed93e3d44c6c20866b2b6ed7515b2ea6
djangouser <ngn2349566@stu.o-hara.ac.jp>
2024-07-29 09:33:18 +0900

    a

--- src/dao/SubjectDao.java
+++ src/dao/SubjectDao.java
@@ -7,9 +7,115 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import bean.School;
 import bean.Subject;
 
 public class SubjectDao extends Dao {
+
+	public Subject get(String cd, School school) throws Exception{
+
+		//科目インスタンスを初期化
+		Subject subject = new Subject();
+		//データベースへのコネクションを確立
+		Connection connection = getConnection();
+		//プリペアードステートメント
+		PreparedStatement statement = null;
+
+		try {
+			//プリペアードステートメントにSQL文をセット
+			statement = connection.prepareStatement("select * from subject where school_cd=? and cd=?");
+			//プリペアードステートメントにデータをバインド
+			statement.setString(1, school.getCd());
+			statement.setString(2, cd);
+			//プリペアードステートメントを実行
+			ResultSet rSet =statement.executeQuery();
+
+			if (rSet.next()) {
+				//リザルトセットが存在する場合
+				//科目インスタンスに検索結果をセット
+				subject.setSchoolCode(rSet.getString("school_cd"));
+				subject.setSubjectCode(rSet.getString("cd"));
+				subject.setSubjectName(rSet.getString("name"));
+			} else {
+				//リザルトセットが存在しない場合
+				//学生インスタンスにnullをセット
+				subject = null;
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			//プリペアードステートメントを閉じる
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+		}
+			//コネクションを閉じる
+			if (connection != null) {
+				try{
+				connection.close();
+			} catch (SQLException sqle) {
+				throw sqle;
+			}
+		}
+		return subject;
+	}
+
+    public List<Subject> filter(School School) throws SQLException {
+        Subject subject = null;
+        List<Subject> list = null;
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = getConnection();
+            statement = connection.prepareStatement("SELECT * FROM SUBJECT WHERE SCHOOL_CD = ?");
+            statement.setString(1, School.getCd());
+            resultSet = statement.executeQuery();
+
+			//リストへの格納処理を実行
+			list = postFilter(resultSet);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return list;
+    }
+
+	private List<Subject> postFilter(ResultSet rSet) throws Exception {
+		//リストを初期化
+		List<Subject> list = new ArrayList<>();
+			try{
+				//リザルトセットを全権走査
+				while (rSet.next()) {
+					//学生インスタンスを初期化
+					Subject subject = new Subject();
+					//学生インスタンスに検索結果をセット
+					subject.setSchoolCode(rSet.getString("SCHOOL_CD"));
+					subject.setSubjectName(rSet.getString("NAME"));
+					subject.setSubjectCode(rSet.getString("CD"));
+					//リストに追加
+					list.add(subject);
+				}
+			} catch (SQLException | NullPointerException e) {
+				e.printStackTrace();
+			}
+			return list;
+	}
+
     public Subject getBySubjectCode(String subjectCode) throws SQLException {
         Subject subject = null;
         Connection connection = null;
