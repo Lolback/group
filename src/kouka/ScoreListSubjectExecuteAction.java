4ebfc85108b0e80edd2f1b987d4183f7b4185b23
djangouser <user@wxample.com>
2024-07-29 11:54:51 +0900

    中本晴輝,成績参照進捗

--- src/kouka/ScoreListSubjectExecuteAction.java
+++ src/kouka/ScoreListSubjectExecuteAction.java
@@ -14,12 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
-import bean.ClassNum;
+import bean.School;
 import bean.Student;
 import bean.Subject;
 import bean.Teacher;
 import bean.TestListSubject;
 import dao.ClassNumDao;
+import dao.TestListSubjectDao;
 import tool.Action;
 
 
@@ -27,26 +28,39 @@ public class ScoreListSubjectExecuteAction  extends Action{
 	   @Override
 	    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
+		   HttpSession session = request.getSession();
+
 
 	        String entYearStr = request.getParameter("academicYear");
+	        int entYear = Integer.parseInt(entYearStr);
 	        String classNum = request.getParameter("class");
-	        String subjectCode = request.getParameter("subject");
+		    Teacher teacher = new Teacher();
+		    teacher = (Teacher) session.getAttribute("current_teacher");
+		    School school = new School();
+		    school = teacher.getSchool();
+		    Subject subject = new Subject();
+
+
 	        int resultCount = 0;
 	        System.out.println(entYearStr);
 	        System.out.println(classNum);
-	        System.out.println(subjectCode);
+	        System.out.println(subject);
 
+	        ClassNumDao cNumDao = new ClassNumDao(); // クラス番号Daoを初期化
+	        TestListSubjectDao tlsubdao = new TestListSubjectDao(); //成績参照daoを初期化
+
+	        List<TestListSubject> testList = new ArrayList<>();
+	        testList = tlsubdao.filter(entYear, classNum, subject, school);
+
+	        TestListSubject test = testList.get(1);
+
+	        System.out.println(test);
 
 	        //いったん持ってくる
-		   HttpSession session = request.getSession();
 	        Connection conn = null;
 	        PreparedStatement pstmt = null;
 	        ResultSet rs = null;
-	        Teacher teacher = new Teacher();
-	        teacher = (Teacher) session.getAttribute("current_teacher");
-	        int entYear = 0; // 入学年度
-	        ClassNumDao cNumDao = new ClassNumDao(); // クラス番号Daoを初期化
-            List<TestListSubject> testList = new ArrayList<>();
+
             List<Student> studentList = new ArrayList<>();
 	        try {
 	            Context initContext = new InitialContext();
@@ -66,71 +80,6 @@ public class ScoreListSubjectExecuteAction  extends Action{
 	 		    * 第二回点数
 	 		    * */
 
-	            // SQLの組み立て
-	            String sql = "SELECT s.ENT_YEAR, s.CLASS_NUM, s.NO, s.NAME, sub.NAME AS SUBJECT_NAME, " +
-	                         "COALESCE(t.POINT, 0) AS POINT, t.NO as TESTNO " +
-	                         "FROM STUDENT s " +
-	                         "JOIN SUBJECT sub ON sub.SCHOOL_CD = s.SCHOOL_CD " +
-	                         "LEFT JOIN TEST t ON s.NO = t.STUDENT_NO AND sub.CD = t.SUBJECT_CD " +
-	                         "WHERE s.SCHOOL_CD = ? AND s.ENT_YEAR = ? AND s.CLASS_NUM = ? AND sub.CD = ?";
-
-
-	            pstmt = conn.prepareStatement(sql);
-
-	            // パラメータの設定
-	            pstmt.setString(1, teacher.getSchool().getCd());
-	            pstmt.setInt(2, entYear);
-	            pstmt.setString(3, classNum);
-	            pstmt.setString(4, subjectCode);
-
-	            rs = pstmt.executeQuery();
-
-
-	            while (rs.next()) {
-	                resultCount++;
-	                Student student = new Student();
-	                TestListSubject tlsub = new TestListSubject();
-	                //tlsub.setEntYear(rs.getInt("ENT_YEAR"));
-	                //tlsub.setClassNum(rs.getString("CLASS_NUM"));
-	                //tlsub.setNo(rs.getString("NO"));
-	                //tlsub.setName(rs.getString("NAME"));
-	                //tlsub.setTestNo(rs.getInt("TESTNO"));
-	                student.setEntYear(rs.getInt("ENT_YEAR"));
-	                student.setClassNum(rs.getString("CLASS_NUM"));
-	                student.setNo(rs.getString("NO"));
-	                student.setName(rs.getString("NAME"));
-	                //student.setTestNo(rs.getInt("TESTNO"));
-
-
-
-	                Subject subject = new Subject();
-	                subject.setSubjectName(rs.getString("SUBJECT_NAME"));
-	                // subject.setCode(rs.getString("SUBJECT_CD")); // もし必要ならば追加
-
-	                // ここでPOINTをStudentオブジェクトに追加する必要がある場合は、student.setPoint(...)を実装する
-
-	                studentList.add(student);
-
-	            }
-	            String stuNameList = rs.getString("NAME");
-	            //クラス番号候補
-	            // ログインユーザーの学校コードをもとにクラス番号の一覧を取得
-	            List<ClassNum> classNumList = cNumDao.filter(teacher.getSchool());
-
-	            // リストを初期化
-	            List<Integer> classNumSet = new ArrayList<>();
-	            // クラス番号をリストに追加
-	            for (int i = 0; i <= classNumList.size() - 1; i++) {
-	            	classNumSet.add(classNumList.get(i).getClassNum());
-	            }
-	            System.out.println(testList);
-	            System.out.println(stuNameList);
-	            System.out.println(classNumSet);
-	            System.out.println(studentList);
-	            request.setAttribute("ent_year_set", entYear);
-	            request.setAttribute("class_num_set", classNumSet);
-	            request.setAttribute("students", studentList);
-	            request.setAttribute("resultCount", resultCount);
 
 	        } catch (Exception e) {
 	            e.printStackTrace();
