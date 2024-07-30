08ebc9204558e372d619847be5b06c6d875ba6a1
ngn2349567@stu.o-hara.ac.jp <ngn2349567@stu.o-hara.ac.jp>
2024-07-30 14:33:27 +0900

    修正

--- src/kouka/StudentCreateExecuteAction.java
+++ src/kouka/StudentCreateExecuteAction.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.School;
 import bean.Student;
 import bean.Teacher;
 import dao.StudentDao;
@@ -18,7 +19,6 @@ public class StudentCreateExecuteAction extends Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response)
 	throws Exception{
 		HttpSession session = request.getSession();//セッション
-        Teacher teacher = new Teacher();
 
 		String entYearStr="";//入力された入学年度
 		String no="";//入力された学生番号
@@ -31,7 +31,9 @@ public class StudentCreateExecuteAction extends Action {
 		StudentDao sDao = new StudentDao();//学生Dao
 		Map<String, String> errors = new HashMap<>();//エラーメッセージ
 
+        Teacher teacher = new Teacher();
         teacher = (Teacher) session.getAttribute("current_teacher");
+        School school = teacher.getSchool();
 
 		//リクエストパラメータの取得 2
 		entYearStr = request.getParameter("f1");
@@ -58,14 +60,14 @@ public class StudentCreateExecuteAction extends Action {
 		student.setSchool(teacher.getSchool());
 
 		//重複検知
-		Student duplicateStudent = sDao.get(no);
+		Student duplicateStudent = sDao.get(no, school);
 		if (duplicateStudent != null) {
 			request.setAttribute("duplicate_flag", true);
 			request.getRequestDispatcher("StudentCreate.action").forward(request, response);
 			return;
 		}
 
-		sDao.save(student);
+		sDao.save(student, school);
 
 		//JSPへフォワード
 		request.getRequestDispatcher("student_add_success.jsp").forward(request, response);
