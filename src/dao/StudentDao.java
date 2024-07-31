1f4cfa39e1d0d61e4e71cffbccea7648631ad182
ngn2349567@stu.o-hara.ac.jp <ngn2349567@stu.o-hara.ac.jp>
2024-07-31 10:58:47 +0900

    学生管理に関する修正

--- src/dao/StudentDao.java
+++ src/dao/StudentDao.java
@@ -269,8 +269,6 @@ public class StudentDao extends Dao {
 		return list;
 	}
 
-
-
 	public boolean save(Student student, School school) throws Exception{
 		//コネクションを確立
 		Connection connection = getConnection();
